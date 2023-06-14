8b8d2684a7246a6aa57328b9d8dfaa4a30eb9646
pancotte <126676608+pancottee@users.noreply.github.com>
2023-06-14 09:04:26 -0300

    up

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+
 // elemento roda jogo
 let canvas = document.getElementById("gamesnake");
 let context = canvas.getContext("2d");
 let box = 32;
+let pontos = 0;
 
 // kobrinha
 let snake = [];
@@ -31,14 +33,14 @@ function criarBG() {
 // cria kobrinha
 function criaCobrinha (){
     for(i = 0; i < snake.length; i++) {
-        context.fillStyle = "green"
+        context.fillStyle = "darkblue"
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
 }
 
 // desenha comida
 function drawFood() {
-    context.fillStyle = "red"
+    context.fillStyle = "darkred"
     context.fillRect(food.x, food.y, box, box)
 }
 
@@ -68,9 +70,11 @@ function iniciarJogo() {
     }
 
     for (i = 1; i < snake.length; i++) {
+        document.getElementById("pnts").innerText = pontos + "pontos"
         if (snake[0].x == snake[i].x && snake[0].y == snake[i].y){
             clearInterval(jogo);
             alert('Game Over >:O')
+            document.getElementById("pnts").innerText = "Após" + pontos + "pontos você perdeu"
         }
     }
 
@@ -92,6 +96,7 @@ function iniciarJogo() {
     } else {
         food.x = Math.floor(Math.random() * 15 +1) * box;
         food.y = Math.floor(Math.random() * 15 +1) * box;
+        pontos += 1
     }
 
     let newHead ={
